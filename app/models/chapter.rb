0cbe6fc98cf85e920dec7b89905e123a7375c2d0
jayaprakash88 <prkash.assi@gmail.com>
2014-12-17 19:37:44 +0530

    Adding Validations

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,4 @@
 class Chapter < ActiveRecord::Base
 	belongs_to :syllabus
+	validates :name, :uniqueness => true,:presence=>true
 end
--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -1,4 +1,5 @@
 class Syllabus < ActiveRecord::Base
 	belongs_to :coures_detail
 	has_many :chapters
+	validates :name, :uniqueness => true,:presence=>true
 end
